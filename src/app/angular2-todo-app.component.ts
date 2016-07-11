34c1d9d3a7eb2909374817a58ed29a044244459d
Sergey Bogdanov <bgrodthai@gmail.com>
2016-07-11 14:46:10 +0300

    Подключена директива app-todo

--- src/app/angular2-todo-app.component.ts
+++ src/app/angular2-todo-app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { TodoAppComponent } from './todo-app/todo-app.component';
 
 @Component({
   moduleId: module.id,
   selector: 'angular2-todo-app-app',
   templateUrl: 'angular2-todo-app.component.html',
-  styleUrls: ['angular2-todo-app.component.css']
+  styleUrls: ['angular2-todo-app.component.css'],
+  directives: [ TodoAppComponent ]
 })
-export class Angular2TodoAppAppComponent {
-  title = 'angular2-todo-app works!';
-}
+export class Angular2TodoAppAppComponent {}
